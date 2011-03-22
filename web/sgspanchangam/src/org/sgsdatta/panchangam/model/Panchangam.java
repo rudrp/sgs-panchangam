e4ff67498772bc646d789b36e7c9fecb9ef827da
sivaprsadhu <sivaprsadhu@gmail.com>
2011-03-22 16:55:46 +0000

    create ne web project

---
+++ web/sgspanchangam/src/org/sgsdatta/panchangam/model/Panchangam.java
@@ -0,0 +1,11 @@
+package org.sgsdatta.panchangam.model;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Unindexed;
+
+
+@Entity
+@Unindexed
+public class Panchangam {
+
+}
